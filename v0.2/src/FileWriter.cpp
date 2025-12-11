26c021a3baeefb25ac01ddc7aa24d97469262b94
osoladosu-collab <os.oladosu@vvk.lt>
2025-12-11 19:22:52 +0200

    Update FileWriter.cpp

--- v0.2/src/FileWriter.cpp
+++ v0.2/src/FileWriter.cpp
@@ -5,7 +5,12 @@
 void FileWriter::writeStudents(const std::string &filename,
                                const std::vector<Student> &students) {
     std::ofstream out(filename);
-    if (!out) throw std::runtime_error("ERROR: Cannot write to file " + filename);
+    if (!out)
+        throw std::runtime_error("ERROR: Cannot write to file " + filename);
+
+    // Buffer for speed
+    static char buffer[1 << 16];
+    out.rdbuf()->pubsetbuf(buffer, sizeof(buffer));
 
     for (const auto &s : students) {
         out << s.getFirstName() << " "
